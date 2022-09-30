6ef528c6ca72cef4a7bfc13e1b0110741bad9b5d
Diego <juandisoca@gmail.com>
2022-09-30 15:09:42 -0600

    ejerc 10

---
+++ 8 - Lonely Integer.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(a) {
+  let lastNumber = a[a.length - 1];
+  let lonely = 0;
+
+  for (let i = 1; i < a.length + 1; i++) {
+    for (let j = 0; j < a.length; j++) {
+      if (a[i] != a[j]) {
+        lonely = a[i];
+      }
+    }
+  }
+  return lonely;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const a = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((aTemp) => parseInt(aTemp, 10));
+
+  const result = lonelyinteger(a);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
